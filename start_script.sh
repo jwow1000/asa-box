2bbc285e0764ddfdba53a4a219dbf03d4f4c788b
jbones1000 <jwilesyoung@gmail.com>
2023-10-15 05:48:42 +0100

    edit start script

--- start_script.sh
+++ start_script.sh
@@ -2,7 +2,7 @@
 
 killall pd
 
-pd -nogui -noadc /home/jwow/asa-box/main_waves.pd /home/jwow/asa-box/mother.pd
+pd -nogui -noadc /home/jwow/asa-box/main_pop.pd /home/jwow/asa-box/mother.pd
 
 exit 0
 
